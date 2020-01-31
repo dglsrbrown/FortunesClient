ade87e7a8b5fe2a18ef93978a03f925287da3996
Douglas Brown <douglasbrown@Douglass-MacBook-Air.local>
2020-01-31 14:31:12 -0500

    url correction

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -5,7 +5,7 @@ switch (window.location.hostname) {
     APIURL = 'http://localhost:4000';
     break;
   case 'drb-blueproject-client.herokuapp.com':
-    APIURL = 'https://drb-blueproject-client.herokuapp.com';
+    APIURL = ' https://drb-fortunesproject-server.herokuapp.com';
 }
 
 export default APIURL;
