dfd0ee3b77842b11ce7c0a09301bfa9526813851
Douglas Brown <douglasbrown@Douglass-MacBook-Air.local>
2020-02-01 23:08:45 -0500

    mvp update

--- src/components/landing/LandingPage.js
+++ src/components/landing/LandingPage.js
@@ -1,19 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Button } from 'reactstrap';
-import Sound from 'react-sound';
-// import '../../assets/intro.mp3'
+import '../../assets/intro.mp3';
 
 const LandingPage = props => {
+
+
+  
+  
+
   return (
     <div>
-      <Sound
-        url='src/assets/intro.mp3'
-        playStatus={Sound.status.PLAYING}
-        // playFromPosition={300 /* in milliseconds */}
-        onLoading={this.handleSongLoading}
-        onPlaying={this.handleSongPlaying}
-        onFinishedPlaying={this.handleSongFinishedPlaying}
-      />
+      {/* <audio ref='audio_tag' src='../../assets/intro.mp3' controls autoPlay/> */}
       <div className='row justify-content-center align-itmes-center'>
         <h2>TEST LANDING PAGE</h2>
       </div>
